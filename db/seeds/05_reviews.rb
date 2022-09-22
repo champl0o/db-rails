27da8310916b334736df4010d6c6d386c2a5c2ac
Champloo <champloo@Champloos-MacBook-Pro.local>
2022-09-23 00:50:17 +0300

    Add small fix to reviews seed

--- db/seeds/05_reviews.rb
+++ db/seeds/05_reviews.rb
@@ -1,8 +1,6 @@
 # frozen_string_literal: true
 
-User.all.each do |_user|
-  Review.create!(
-    description: Faker::Books::Dune.quote,
-    reviewable: Item.last
-  )
-end
+Review.create!(
+  description: Faker::Books::Dune.quote,
+  reviewable: Item.last
+)
